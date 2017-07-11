baa6e7ba719495419096824218c83942f11260f1
heaenwie <marta.eleonora.ciesielska@gmail.com>
2017-07-11 18:01:24 +0200

    Add breaks

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 TODO:
 Pomodoro timer musi mieć następujące funkcje:
-1. Odliczanie 25 minut i sekund
-2. Zatrzymanie się na 0
-3. Po 25 minutach, przerwa, trwająca 5 minut.
-4. Przyciski: START | STOP | PAUZA
+1. Odliczanie 25 minut i sekund -> Done
+2. Zatrzymanie się na 0 -> Done
+3. Po 25 minutach, przerwa, trwająca 5 minut -> Done
+4. Przyciski: START | STOP | PAUZA -> Done
 5. Przyciski góra / dół przy czasomierzu, aby móc zmienić czas.
 6. Opcjonalnie - zapisywanie liczby wykonanych pomidorków.
 7. Opcjonalnie - dodanie nazwy tego, nad czym pracujesz.
 8. Opcjonalnie - blokowanie określonych adresów www w czasie trwania timera.
 9. Opcjonalnie - Synchronizację z Todoistem [fetch danych]
-10. Opcjonalnie - Synchronizację z Mailem & Google Calendarem. 
+10. Opcjonalnie - Synchronizację z Mailem & Google Calendarem.
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var distance = new Date();
-// z jakiegoś powodu się nie liczy!
 var count = 0;
+var y;
+
 function pomodoroTimer (initMinutes, initSeconds)  {
 
   // Find the distance between now an the count down date
@@ -15,12 +16,28 @@ function pomodoroTimer (initMinutes, initSeconds)  {
   document.getElementById("timer").innerHTML = minutes + " : " + seconds;
 
   // If the count down is finished, write some text
-  console.log("distance = " + distance);
-  console.log("distance condition " + (distance < 0));
   if (distance.getMinutes() == 0 && distance.getSeconds() == 0) {
     clearInterval(x);
     count = 0;
-    document.getElementById("timer").innerHTML = "EXPIRED";
+    document.getElementById("status").innerHTML = "Break Time";
+    y = setInterval (function(){breakTime()}, 1000);
   }
-  // console.log("outside x = " + x);
+}
+
+function breakTime() {
+  distance.setMinutes(0);
+  distance.setSeconds(5-count);
+  count++;
+
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  document.getElementById("timer").innerHTML = minutes + " : " + seconds;
+
+  if (distance.getMinutes() == 0 && distance.getSeconds() == 0) {
+    document.getElementById("status").innerHTML = "Work time";
+    clearInterval(y);
+    count = 0;
+  }
+
 }
